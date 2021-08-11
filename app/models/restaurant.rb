5fc3782875461e21ee997047acca34fc15ebc087
Quill Cronwall <quilljazz@gmail.com>
2021-08-11 12:41:51 +0900

    validations almost complete

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  CATEGORIES =  [:chinese, :italian, :japanese, :french, :belgian ] #inclusion validation
+  #validates :category, inclusion: { in: CATEGORIES }
+end
